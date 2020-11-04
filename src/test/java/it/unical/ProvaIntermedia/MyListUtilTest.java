dc665194dc3c356a551f802ba2b35fa849f21c34
Santino Locanto <santino.locanto2015@libero.it>
2020-11-04 11:32:37 +0100

    Modify MyListUtilTest

--- src/test/java/it/unical/ProvaIntermedia/MyListUtilTest.java
+++ src/test/java/it/unical/ProvaIntermedia/MyListUtilTest.java
@@ -1,5 +1,37 @@
 package it.unical.ProvaIntermedia;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class MyListUtilTest {
 	
+	private static MyListUtil list;
+	
+	@BeforeClass
+	public static void prepare() {
+		list = new MyListUtil();
+	}
+	
+	@Before
+	public void init() {
+		System.out.println("init test");
+	}
+	
+	@Test
+	public void ordineCrescenteWorks() {
+		List<Integer> array = Arrays.asList(3,4,2,5,1,6);
+		assertEquals(Arrays.asList(1,2,3,4,5,6), list.ordineCrescente(array));
+	}
+	
+	@Test
+	public void ordineDecrescenteWorks() {
+		List<Integer> array = Arrays.asList(3,4,2,5,1,6);
+		assertEquals(Arrays.asList(6,5,4,3,2,1), list.ordineDecrescente(array));
+	}
 }
